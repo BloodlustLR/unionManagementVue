e10523cc4b1a0f0cf9b8b4b8d350bcf61758c329
Pokenomon <yac3526641@163.com>
2021-12-12 13:55:38 +0800

    提交补损标准模块

--- src/views/Menu/sys/ship.vue
+++ src/views/Menu/sys/ship.vue
@@ -2,7 +2,7 @@
 <div class="ship">
     <div class="action-board">
 
-        <el-button type="success" @click="" style="margin-right:240px;">新增舰船</el-button>
+        <el-button type="success" @click="openAddShipModal" style="margin-right:240px;">新增舰船</el-button>
 
         科技等级:
         <el-select v-model="filter.level" placeholder="选择科技等级" style="width:200px;margin-right:20px;" filterable>
@@ -17,10 +17,11 @@
             <el-option label="驱逐舰" value="驱逐舰"></el-option>
             <el-option label="巡洋舰" value="巡洋舰"></el-option>
             <el-option label="战列巡洋舰" value="战列巡洋舰"></el-option>
+            <el-option label="战列舰" value="战列舰"></el-option>
             <el-option label="旗舰" value="旗舰"></el-option>
         </el-select>
 
-        <el-button type="primary" @click="">查询</el-button>
+        <el-button type="primary" @click="filterAll">查询</el-button>
 
     </div>
     <div class="main-board">
@@ -34,36 +35,231 @@
             <el-table-column prop="name" label="舰船名"/>
             <el-table-column label="操作" width="300">
                 <template #default="scope">
-                    <!-- <span style="margin-left: 10px">{{ scope.row.date }}</span> -->
-                    <el-button type="primary" size="small">修改</el-button>
-                    <el-button type="danger" size="small">删除</el-button>
+                    <el-button type="primary" size="small" @click="openConfigShipModal(scope.row)">修改</el-button>
+                    <el-button type="danger" size="small" @click="removeShip(scope.row.id)">删除</el-button>
                 </template>
             </el-table-column>
         </el-table>
     </div>
     <div class="bottom-board">
-          <el-pagination :page-size="20" :pager-count="11" layout="prev, pager, next" :total="1000"></el-pagination>
+        <el-pagination :page-size="filter.pageSize" layout="prev, pager, next" :total="filter.pageTotal" @current-change="filterAll"></el-pagination>
     </div>
+
+    <el-dialog v-model="addShipModal" title="新增舰船" width="500px" :close-on-click-modal="false">
+        <div>
+            <div style="width:400px;margin:0 auto;text-align:center;height:60px;line-height:60px">
+                科技等级:<el-select v-model="shipInfo.level" placeholder="选择科技等级" style="width:200px;margin-left:10px;" filterable>
+                            <el-option v-for="(item,index) in 10" :key="index" :label="'T'+item" :value="item"></el-option>
+                        </el-select>
+            </div>
+            <div style="width:400px;margin:0 auto;text-align:center;height:60px;line-height:60px">
+                舰船类型:<el-select v-model="shipInfo.type" placeholder="选择舰船类型" style="width:200px;margin-left:10px;" filterable>
+                            <el-option label="护卫舰" value="护卫舰"></el-option>
+                            <el-option label="驱逐舰" value="驱逐舰"></el-option>
+                            <el-option label="巡洋舰" value="巡洋舰"></el-option>
+                            <el-option label="战列巡洋舰" value="战列巡洋舰"></el-option>
+                            <el-option label="战列舰" value="战列舰"></el-option>
+                            <el-option label="旗舰" value="旗舰"></el-option>
+                        </el-select>
+            </div>
+            <div style="width:400px;margin:0 auto;text-align:center;height:60px;line-height:60px">
+                舰船名称:<el-input v-model="shipInfo.name" placeholder="请输入舰船名称" style="width:200px;margin-left:10px;"/>
+            </div>
+        </div>
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="addArmyModal = false">取消</el-button>
+                <el-button type="primary" @click="addShip">新增</el-button>
+            </span>
+        </template>
+    </el-dialog>
+
+    <el-dialog v-model="configShipModal" title="配置舰船" width="500px" :close-on-click-modal="false">
+        <div>
+            <div style="width:400px;margin:0 auto;text-align:center;height:60px;line-height:60px">
+                科技等级:<el-select v-model="shipInfo.level" placeholder="选择科技等级" style="width:200px;margin-left:10px;" filterable>
+                            <el-option v-for="(item,index) in 10" :key="index" :label="'T'+item" :value="item"></el-option>
+                        </el-select>
+            </div>
+            <div style="width:400px;margin:0 auto;text-align:center;height:60px;line-height:60px">
+                舰船类型:<el-select v-model="shipInfo.type" placeholder="选择舰船类型" style="width:200px;margin-left:10px;" filterable>
+                            <el-option label="护卫舰" value="护卫舰"></el-option>
+                            <el-option label="驱逐舰" value="驱逐舰"></el-option>
+                            <el-option label="巡洋舰" value="巡洋舰"></el-option>
+                            <el-option label="战列巡洋舰" value="战列巡洋舰"></el-option>
+                            <el-option label="旗舰" value="旗舰"></el-option>
+                        </el-select>
+            </div>
+            <div style="width:400px;margin:0 auto;text-align:center;height:60px;line-height:60px">
+                舰船名称:<el-input v-model="shipInfo.name" placeholder="请输入舰船名称" style="width:200px;margin-left:10px;"/>
+            </div>
+        </div>
+        <template #footer>
+            <span class="dialog-footer">
+                <el-button @click="configShipModal = false">取消</el-button>
+                <el-button type="primary" @click="configShip">确认</el-button>
+            </span>
+        </template>
+    </el-dialog>
+
 </div>
 </template>
 <script>
+import { ElMessageBox,ElMessage } from 'element-plus'
+
 export default {
     data(){
         return{
             filter:{
                 level:"0",
-                type:'全部'
+                type:'全部',
+                pageNum:1,
+                pageSize:10,
+                pageTotal:0
             },
-            tableData:[
-                {
-                    id:1,
-                    level:9,
-                    type:'战列巡洋舰',
-                    name:'幼龙级'
+            tableData:[],
+
+            addShipModal:false,
+            configShipModal:false,
+            shipInfo:{
+                id:null,
+                level:1,
+                type:'护卫舰',
+                name:''
+            }
+        }
+    },
+    created(){
+        this.filterAll();
+    },
+    mounted(){
+        
+    },
+    methods:{
+        filterAll(pageNum){
+            if(pageNum && pageNum instanceof Number){
+                this.filter.pageNum = pageNum;
+            }
+            this.$request.post("/ship/pageShip",this.filter).then(res =>{
+                this.tableData = res.obj.records;
+                this.filter.pageNum = res.obj.current;
+                this.filter.pageSize = res.obj.size;
+                this.filter.pageTotal = res.obj.total;
+                console.log(res.obj);
+                console.log(this.filter);
+            })
+        },
+
+        openAddShipModal(){
+            this.shipInfo={
+                id:null,
+                level:1,
+                type:'护卫舰',
+                name:''
+            }
+            this.addShipModal = true;
+        },
+
+        addShip(){
+            if(isEmpty(this.shipInfo.name)){
+                ElMessage({
+                    message: '请先补全数据',
+                    type: 'warning',
+                })
+                return;
+            }
+            this.$request.post("/ship/addShip",this.shipInfo).then(res =>{
+                if(res.obj=="success"){
+                    ElMessage({
+                        message: '新增成功',
+                        type: 'success',
+                    })
+                    this.addShipModal = false;
+                    this.filterAll();
+                }else{
+                    ElMessage.error('新增失败');
                 }
-            ]
+            })
+        },
+
+        openConfigShipModal(data){
+            this.shipInfo={
+                id:data.id,
+                level:data.level,
+                type:data.type,
+                name:data.name
+            }
+            this.configShipModal = true;
+        },
+
+        configShip(){
+            if(isEmpty(this.shipInfo.name)){
+                ElMessage({
+                    message: '请先补全数据',
+                    type: 'warning',
+                })
+                return;
+            }
+            this.$request.post("/ship/configShip",this.shipInfo).then(res => {
+                if(res.obj=="success"){
+                    ElMessage({
+                        message: '配置成功',
+                        type: 'success',
+                    })
+                    this.configShipModal = false;
+                    this.filterAll();
+                }else{
+                    ElMessage.error('配置失败');
+                }
+            })
+        },
+
+        removeShip(id){
+            ElMessageBox.confirm(
+                '是否确认删除此舰船?',
+                '删除舰船',
+                {
+                    confirmButtonText: '确认',
+                    cancelButtonText: '取消',
+                    type: 'warning',
+                }
+            )
+            .then(() => {
+                this.$request.post("/ship/removeShip",{
+                    shipId:id
+                }).then(res => {
+                    if(res.obj=="success"){
+                        ElMessage({
+                            message: '删除成功',
+                            type: 'success',
+                        })
+                    }else{
+                        ElMessage.error('删除失败');
+                    }
+                    this.filterAll();
+                })
+            }).catch(() => {
+
+            })
         }
     }
+}
+/**
+ * 判断数据是否为空
+ * @param value 需要验证的数据
+ * @returns {boolean}
+ */
+function isEmpty(value) {
+  if (
+    value === undefined ||
+    value === null ||
+    value === "" ||
+    value.toString().trim().length === 0
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 </script>
 <style lang="less">
--- src/views/Menu/union/FillLoss.vue
+++ src/views/Menu/union/FillLoss.vue
@@ -2,7 +2,7 @@
 <div class="fill-loss">
     <div class="action-board">
         <el-button type="primary" @click="">新建补损</el-button>
-        <el-button type="success" @click="standardModal=true" style="margin-right:240px;">补损标准</el-button>
+        <el-button type="success" @click="openStandardPaymentModal" style="margin-right:240px;">补损标准</el-button>
 
         <el-date-picker v-model="filter.dateRange" type="daterange" range-separator="至" start-placeholder="起始日期" end-placeholder="结束日期"></el-date-picker>
         <el-button type="primary" @click="" style="margin-left:20px">查询</el-button>
@@ -12,10 +12,11 @@
             <el-table-column prop="date" label="日期"/>
             <el-table-column prop="name" label="补损名"/>
             <el-table-column prop="endTime" label="截止时间"/>
+            <el-table-column prop="state" label="状态"/>
             <el-table-column label="操作" width="300">
                 <template #default="scope">
                     <!-- <span style="margin-left: 10px">{{ scope.row.date }}</span> -->
-                    <el-button type="success" size="small" @click="">复制链接</el-button>
+                    <el-button type="success" size="small" @click="copyLink">复制链接</el-button>
                     <el-button type="primary" size="small" @click="checkDetail">查看详情</el-button>
                     <el-button type="danger" size="small">删除</el-button>
                 </template>
@@ -30,18 +31,19 @@
         <div style="height:500px;width:1100px;margin:0 auto">
             <div style="float:left;height:498px;width:198px;border:1px solid black;">
                 <div style="height:calc(100% - 40px);width:100%;overflow-y:auto;">
-                    <div v-for="(item,index) in standardList" :key="index" class="standard-item">{{item.name}}</div>
+                    <div v-for="(item,index) in standardList" :key="index" class="standard-item" @click="loadStandard(item)">{{item.name}}</div>
                 </div>
                 <div style="height:40px;line-height:40px;width:100%;text-align:center;">
-                    <el-button type="success" size="mini">新建标准</el-button>
+                    <el-button type="success" size="mini" @click="addStandardPayment">新建标准</el-button>
                 </div>
             </div>
-            <div style="float:right;height:500px;width:900px;">
+            <div style="float:right;height:500px;width:900px;" v-show="standardInfo.id!=null">
                 <div style="padding-left:30px;height:59px;line-height:59px;border-bottom:1px solid black">
-                    补损金额:  <el-input-number v-model="standardInfo.num" :min="1" size="small" controls-position="right"/>
+                    <span style="margin-right:50px;font-weight:bolder">{{standardInfo.name}}</span>补损金额:  <el-input-number v-model="standardInfo.num" :min="1" size="small" controls-position="right"/>
+                    <el-button type="primary" size="small" style="margin-left:50px;" @click="configStandardPayment">提交</el-button>
                 </div>
                 <div style="margin-top:10px;padding-left:30px;height:540px;">
-                    <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick" />
+                    <el-tree ref="shipTree" node-key="id" :data="shipTree" show-checkbox @check-change="handleCheckChange"/>
                 </div>
             </div>
             <div class="clear"></div>
@@ -55,26 +57,21 @@
 </div>
 </template>
 <script>
+import { ElMessageBox,ElMessage } from 'element-plus'
+
 export default {
     data(){
         return{
 
             standardModal:false,
             standardInfo:{
-                num:10000
+                id:null,
+                name:'',
+                num:0,
+                shipList:[]
             },
-            standardList:[
-                {
-                    id:1,
-                    name:'T8战巡',
-                    num:10000
-                },
-                {
-                    id:1,
-                    name:'T7战巡',
-                    num:10000
-                }
-            ],
+            standardList:[],
+            shipTree:[],
 
 
             filter:{
@@ -125,11 +122,154 @@ export default {
             ]
         }
     },
+    mounted(){
+        this.getShipTree();
+    },
     methods:{
         checkDetail(){
             this.$router.push("/home/filllossDetail");
+        },
+
+        copyLink(){
+            copyText("123");
+            ElMessage({
+                message: '补损链接已复制到剪贴板',
+                type: 'success'
+            })
+        },
+
+        getStandardPayment(){
+            this.$request.get("/standardPayment/getAllStandardPayment").then(res => {
+                console.log(res.obj);
+                this.standardList = res.obj;
+            })
+        },
+
+        getShipTree(){
+            this.$request.get("/ship/getShipTree").then(res => {
+                console.log(res.obj);
+                this.shipTree = res.obj;
+                this.getStandardPayment();
+            })
+        },
+
+
+        openStandardPaymentModal(){
+            this.standardInfo.id = null;
+            this.standardInfo.num = 0;
+            this.standardInfo.name = "";
+            this.standardInfo.shipList = [];
+            this.standardModal = true;
+        },
+
+        handleCheckChange(){
+            let checked = this.$refs.shipTree.getCheckedNodes();
+            let newChecked = [];
+            for(let item of checked){
+                if(!isNaN(Number(item.id))){
+                    newChecked.push(item.id);
+                }
+            }
+            this.standardInfo.shipList = newChecked;
+            console.log(this.standardInfo.shipList);
+        },
+
+        addStandardPayment(){
+            ElMessageBox.prompt('请输入你的补损标准名称', '新建补损标准', {
+                confirmButtonText: '确认',
+                cancelButtonText: '取消',
+            }).then(({ value }) => {
+                if(isEmpty(value)){
+                    ElMessage({
+                        message: '请先补全数据',
+                        type: 'warning',
+                    })
+                    return;
+                }
+                this.$request.post("/standardPayment/addStandardPayment",{
+                    name:value
+                }).then(res =>{
+                    if(res.obj=="success"){
+                        ElMessage({
+                            message: '新增成功',
+                            type: 'success',
+                        })
+                        this.getStandardPayment();
+                    }else{
+                        ElMessage.error('新增失败');
+                    }
+                })
+            }).catch(() => {
+
+            })
+        },
+
+        loadStandard(value){
+            this.resetTree();
+            this.standardInfo.id = value.id;
+            this.standardInfo.name = value.name;
+            this.standardInfo.num = value.num;
+            let shipList = [];
+            for(let item of value.relatedShipList){
+                shipList.push(item.id);
+            }
+            this.standardInfo.shipList = shipList;
+            this.$refs.shipTree.setCheckedKeys(shipList);
+        },
+
+        configStandardPayment(){
+            let data = {
+                id:this.standardInfo.id,
+                num:this.standardInfo.num,
+                shipList:this.standardInfo.shipList
+            }
+            this.$request.post("/standardPayment/configStandardPayment",data).then(res =>{
+                if(res.obj=="success"){
+                    ElMessage({
+                        message: '配置成功',
+                        type: 'success',
+                    })
+                    this.getStandardPayment();
+                }else{
+                    ElMessage.error('配置失败');
+                }
+            })
+        },
+
+        resetTree(){
+            this.$refs.shipTree.setCheckedKeys([]);
         }
     }
+}
+
+// 复制的方法
+function copyText(text, callback){ // text: 要复制的内容， callback: 回调
+    var tag = document.createElement('input');
+    tag.setAttribute('id', 'cp_hgz_input');
+    tag.value = text;
+    document.getElementsByTagName('body')[0].appendChild(tag);
+    document.getElementById('cp_hgz_input').select();
+    document.execCommand('copy');
+    document.getElementById('cp_hgz_input').remove();
+    if(callback) {callback(text)}
+}
+
+/**
+ * 判断数据是否为空
+ * @param value 需要验证的数据
+ * @returns {boolean}
+ */
+function isEmpty(value) {
+  if (
+    value === undefined ||
+    value === null ||
+    value === "" ||
+    value.toString().trim().length === 0
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 }
 </script>
 <style lang="less">
